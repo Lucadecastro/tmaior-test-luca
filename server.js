fe6aa285c9877f3b55671973b6375fc20e5a75d5
Luca Castro <lucadecastro@gmail.com>
2023-11-08 23:52:02 -0300

    add cors

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 app.use(express.static(__dirname));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
 
