097e03a11dbd5e872cd448f96b880ac95ff7fa31
Luca Castro <luucadecastro@gmail.com>
2023-11-09 18:22:42 -0300

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,4 +5,4 @@ docker stop tmaior-chat-app-container || true
 docker rm tmaior-chat-app-container || true
 
 # Start the new container
-docker run -d --name tmaior-chat-app-container -p 80:80 lucadecastro/tmaior-chat-image:v1
+docker run -d --name tmaior-chat-app-container -p 8080:80 lucadecastro/tmaior-chat-image:v1
